50af4bce4c47865b40ccb8018b526f54aef58f31
claytonsuplinski <claytonsuplinski@gmail.com>
2015-08-30 22:44:59 -0500

    Working on Pitch Detection

--- assets/js/AudioManager.js
+++ assets/js/AudioManager.js
@@ -1,18 +1,44 @@
-function AudioManager(src){
+function AudioManager(src){	
 	var audio_hidden = new Audio(src);
-	var context = new AudioContext();
+	
+	this.context = new AudioContext();
 
 	var audioElement = audio_hidden;
-	var mediaSourceNode = context.createMediaElementSource(audioElement);
+	var mediaSourceNode = this.context.createMediaElementSource(audioElement);
 
 	var self = this;
-	this.analyser = context.createAnalyser();
+	this.analyser = this.context.createAnalyser();
 	mediaSourceNode.connect(self.analyser);
-	this.analyser.connect(context.destination);
+	this.analyser.connect(this.context.destination);
 
 	this.analyser.fftSize = 32;
 	var bufferLength = this.analyser.frequencyBinCount;
 	this.dataArray = new Float32Array(bufferLength);
+	
+	///////////pitch detect///////////////
+	
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+	theBuffer = null;
+	
+	context = this.context;
+	var request = new XMLHttpRequest();
+	request.open("GET", src, true);
+	request.responseType = "arraybuffer";
+	request.onload = function() {
+	  context.decodeAudioData( request.response, function(buffer) { 
+	    	theBuffer = buffer;
+		} );
+	}
+	request.send();
+	
+	detectorElem = document.getElementById( "detector" );
+	pitchElem = document.getElementById( "pitch" );
+	noteElem = document.getElementById( "note" );
+	detuneElem = document.getElementById( "detune" );
+	detuneAmount = document.getElementById( "detune_amt" );
+	
+	/////////end pitch detect/////////////
 
 	audioElement.play();
 	audioElement.volume=0.001;
@@ -30,12 +56,12 @@ function AudioManager(src){
 }
 
 AudioManager.prototype.analyze = function(){
-
 		TTR.now = (new Date()).getTime();
 	
 		var self = this;
 		
 		self.analyser.getFloatFrequencyData(this.dataArray);
+		
 		var tmp_html = "";
 		var curr_data = {};
 		curr_data.left   = 0;
@@ -92,3 +118,116 @@ var now = new Date();
 var tmp_offset = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds()+1, 0) - now;
 
 setTimeout(function(){new AudioManager("./assets/audio/in_paris.mp3");}, tmp_offset);
+
+
+///////////////////////
+///////////////////////
+///////////////////////
+///////////////////////
+///////////////////////
+///////////////////////
+///////////////////////
+///////////////////////
+
+
+function togglePlayback() {
+    sourceNode = context.createBufferSource();
+    sourceNode.buffer = theBuffer;
+
+    analyser = context.createAnalyser();
+    analyser.fftSize = 2048;
+    sourceNode.connect( analyser );
+    analyser.connect( context.destination );
+    sourceNode.start( 0 );
+    updatePitch();
+}
+
+var buffer_length = 1024;
+var buf = new Float32Array( buffer_length );
+
+var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+function noteFromPitch( frequency ) {
+	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+	return Math.round( noteNum ) + 69;
+}
+
+function frequencyFromNoteNumber( note ) {
+	return 440 * Math.pow(2,(note-69)/12);
+}
+
+function centsOffFromPitch( frequency, note ) {
+	return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
+}
+
+function autoCorrelate( buf, sampleRate ) {
+	var SIZE = buf.length;
+	var MAX_SAMPLES = Math.floor(SIZE/2);
+	var best_offset = -1;
+	var best_correlation = 0;
+	var rms = 0;
+	var foundGoodCorrelation = false;
+	var correlations = new Array(MAX_SAMPLES);
+
+	for (var i=0;i<SIZE;i++) {
+		var val = buf[i];
+		rms += val*val;
+	}
+	rms = Math.sqrt(rms/SIZE);
+	if (rms<0.01) // not enough signal
+		return -1;
+
+	var previous_correlation=1;
+	for (var offset = 0; offset < MAX_SAMPLES; offset++) {
+		var correlation = 0;
+
+		for (var i=0; i<MAX_SAMPLES; i++) {
+			correlation += Math.abs((buf[i])-(buf[i+offset]));
+		}
+		correlation = 1 - (correlation/MAX_SAMPLES);
+		correlations[offset] = correlation; // store it, for the tweaking we need to do below.
+		if ((correlation>0.9) && (correlation > previous_correlation)) {
+			foundGoodCorrelation = true;
+			if (correlation > best_correlation) {
+				best_correlation = correlation;
+				best_offset = offset;
+			}
+		} else if (foundGoodCorrelation) {
+			var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];  
+			return sampleRate/(best_offset+(8*shift));
+		}
+		previous_correlation = correlation;
+	}
+	if (best_correlation > 0.01) {
+		return sampleRate/best_offset;
+	}
+	return -1;
+}
+
+function updatePitch( time ) {
+	analyser.getFloatTimeDomainData( buf );
+	var ac = autoCorrelate( buf, context.sampleRate );
+	
+ 	if (ac != -1) {
+	 	detectorElem.className = "confident";
+	 	pitch = ac;
+	 	pitchElem.innerHTML = Math.round( pitch ) ;
+	 	var note =  noteFromPitch( pitch );
+		noteElem.innerHTML = noteStrings[note%12];
+		var detune = centsOffFromPitch( pitch, note );
+		if (detune == 0 ) {
+			detuneElem.className = "";
+			detuneAmount.innerHTML = "--";
+		} else {
+			if (detune < 0)
+				detuneElem.className = "flat";
+			else
+				detuneElem.className = "sharp";
+			detuneAmount.innerHTML = Math.abs( detune );
+		}
+	}
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+	window.requestAnimationFrame( updatePitch );
+}
